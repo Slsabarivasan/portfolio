a23ae68a5ba6d82dc7050e4e3a00eca2ed3f4731
Slsabarivasan <slsabarivasan01@gmail.com>
2025-03-17 14:53:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector("#menu");
+const nav = document.querySelector(".links");
+
+menu.onclick =()=>{
+  menu.classList.toggle('bx-x');
+  nav.classList.toggle('active'); 
+}
+
+
+ 
